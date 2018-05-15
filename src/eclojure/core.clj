53ca1d5c99ae933e4bf46e90fcbb252cbb9022e1
Ruslan Kiselev <voltecrus@gmail.com>
2018-05-15 21:24:11 +0300

    Added logic for assoc files and code

--- src/eclojure/core.clj
+++ src/eclojure/core.clj
@@ -1,15 +1,17 @@
 (ns eclojure.core
   (:require [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 ;; find all files in the directory which are clj or cljs
 
 (defn find-project-files [path pattern]
-  (let [clj-files (filter #(-> % (.getName) (.contains pattern))
-                          (let [directory (clojure.java.io/file path)]
-                            (file-seq directory)))]
-    (-> clj-files first io/reader line-seq)))
+  (filter #(-> % (.getName) (.contains pattern))
+          (let [directory (clojure.java.io/file path)]
+            (file-seq directory))))
+
+(reduce (fn [map file]
+          (assoc map (.getName file) (-> file
+                                         io/reader line-seq)))
+        {}
+        (find-project-files "." "clj"))
+
+()
